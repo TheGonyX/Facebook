7854a2ea2c0d6560b4d8a5b0650ed3900045a98a
Martin Uvíra <uvira.martin@st.spsoa.cz>
2017-06-01 22:20:16 +0200

    git remote add origin https://github.com/TheGonyX/Facebook.git git push -u origin master

---
+++ Facebook/Facebook/FB.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono;
+
+int main() {
+
+	string jmeno;
+	string heslo="1";
+	string heslo2="2";
+	string opakovat;
+	system("Color 1F");
+
+	cout << "zvolte sve uzivatelske jmeno: " << endl;
+	cin >> jmeno;
+	while (heslo != heslo2) {
+		system("cls");
+		cout << "Zadejte heslo: " << endl;
+		cin >> heslo;
+		cout << "Zopakujte heslo: " << endl;
+		cin >> heslo2;
+		if (heslo != heslo2)				{ 
+			cout << "Nespravne heslo" << endl;
+			sleep_until(system_clock::now() + seconds(1));
+		}
+
+							}
+	system("cls");
+
+
+
+
+
+	cout <<"\t \t \t \t Dekujeme za vytvoreni vaseho uctu Facebook" << endl;
+	sleep_until(system_clock::now() + seconds(2));
+	system("cls");
+	cout << "\t \t \t \t \t Koho mozna znate" << endl;
+	cout << "\t \t \t \t \t Jakub Ce" << endl;
+	cout << "\t \t \t \t \t Milos Zeman" << endl;
+	cout << "\t \t \t \t \t Bohuslav Sobotka" << endl;
+	cout << "\t \t \t \t \t Miroslav Kalousek" << endl;
+	cout << "\t \t \t \t \t Radomir Matonoha" << endl;
+	cout << "\t \t \t \t \t Oldrich Seidler" << endl;
+	cout << "\t \t \t \t \t Dominik Holusa " << endl;
+	cout << "\t \t \t \t \t Martin Uvira" << endl;
+
+
+
+
+
+
+
+	system("pause");
+}
